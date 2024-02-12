e79ec8312039f79b5b2280f68ae93b34bf89bea9
Keren Cohen <keren.c91@gmail.com>
2024-02-12 23:44:04 +0200

    some adaptations in utils - pe, arrange

--- bs_inversion/utils.py
+++ bs_inversion/utils.py
@@ -2,6 +2,62 @@ import torch
 import numpy as np
 import torch
 import torch.nn as nn
+import math
+
+# add positional encoding as concatanation of data: [1, 2, 10, 10] --> [1, 2, 20, 10]
+def perform_positional_encoding2(x, d_model):
+    max_length = x.size(-1)#10 # length of sequence, here length of signal
+    n = 10000.0
+    pe = torch.zeros(max_length, d_model)    
+
+    # create position column   
+    k = torch.arange(0, max_length).unsqueeze(1)  
+
+    # calc divisor for positional encoding 
+    div_term = torch.exp(                                 
+            torch.arange(0, d_model, 2) * -(math.log(n) / d_model)
+    )
+
+    # calc sine on even indices
+    pe[:, 0::2] = torch.sin(k * div_term)    
+
+    # calc cosine on odd indices   
+    pe[:, 1::2] = torch.cos(k * div_term)  
+
+    # add dimension     
+    pe = pe.unsqueeze(0).unsqueeze(0)
+    pe = pe.repeat(1, 2, 1, 1)
+    stacked_tensor = torch.cat([x, pe], dim=2)
+    
+    return stacked_tensor 
+
+
+# add positional ecoding as additional channel: [1, 2, 10, 10] --> [1, 3, 10, 10]
+def perform_positional_encoding(x, d_model):
+    max_length = x.size(2)#10 # length of sequence, here length of signal
+    #d_model = x.size(-1)
+    n = 10000.0
+    pe = torch.zeros(max_length, d_model)    
+
+    # create position column   
+    k = torch.arange(0, max_length).unsqueeze(1)  
+
+    # calc divisor for positional encoding 
+    div_term = torch.exp(                                 
+            torch.arange(0, d_model, 2) * -(math.log(n) / d_model)
+    )
+
+    # calc sine on even indices
+    pe[:, 0::2] = torch.sin(k * div_term)    
+
+    # calc cosine on odd indices   
+    pe[:, 1::2] = torch.cos(k * div_term)  
+
+    # add dimension     
+    pe = pe.unsqueeze(0).unsqueeze(0)
+    stacked_tensor = torch.cat([x, pe], dim=1)
+    
+    return stacked_tensor   
 
 def calculate_bispectrum_power_spectrum_efficient(x, dt=1.):
     """
@@ -80,6 +136,9 @@ class BispectrumCalculator(nn.Module):
         self.batch_size = batch_size
         self.target_len = target_len
         self.device = device
+        self.channels = 2
+        self.height = target_len
+        self.width = target_len
         
     def _create_data(self, target):
         # Create data
@@ -88,13 +147,13 @@ class BispectrumCalculator(nn.Module):
         bs_real = bs.real.float()
         bs_imag = bs.imag.float()
         source = torch.stack([bs_real, bs_imag], dim=1)
-        
+               
         return source, target.unsqueeze(0) 
     # target: signal 1Xtarget_len
     # source: bs     2Xtarget_lenXtarget_len
     def forward(self, target):
         # Iterate over the batch dimension using indexing
-        source = torch.zeros(self.batch_size, 2, self.target_len, self.target_len).to(self.device)
+        source = torch.zeros(self.batch_size, self.channels, self.height, self.width).to(self.device)
 
         for i in range(self.batch_size):
             source[i], target[i] = self._create_data(target[i])
