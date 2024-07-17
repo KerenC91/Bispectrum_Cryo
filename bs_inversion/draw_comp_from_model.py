44657edf71ba713724407bbd75cf7e99e2e687f8
Keren Cohen <keren.c91@gmail.com>
2024-07-17 14:08:16 +0300

    Update offline drawing script

--- bs_inversion/draw_comp_from_model.py
+++ bs_inversion/draw_comp_from_model.py
@@ -40,10 +40,11 @@ args = parser.parse_args()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # Set args
 baseline_data_folder = f'baseline_K_{args.K}_N_{args.N}'
-model_folder = 'test_K_2_offline_ep7800'
-test_folder = 'test_K_2_offline_ep7800'
+model_folder = 'test_K_2_N_20_average'
+test_folder = 'test_K_2_N_20_average111'
 N = args.N
 K = args.K
+check_k1_k2_distance = False
 # Set baeline data path
 baseline_data_path = os.path.join(os.path.join(hparams.data_root, 'baseline_data'),
                                   baseline_data_folder)
@@ -79,32 +80,60 @@ def plot_output_debug2(target, output, folder, from_matlab=None):
   
     plt.figure()
     plt.title('Comparison between original signal and its reconstructions')
-    plt.plot(target, label='org')
-    plt.plot(output, label='tested')
+    plt.plot(output, label='tested', color='tab:orange')
     if from_matlab is not None:
-        plt.plot(from_matlab, label='baseline')
+        plt.plot(from_matlab, label='baseline', color='tab:green')
+    plt.plot(target, label='org', color='tab:blue')
     plt.ylabel('signal')
     plt.xlabel('time')
     plt.legend()
     plt.savefig(fig_path)        
     plt.close()
-        
     
-# load the model
+def switch_position(pred, target, bs_calc):
+    switch = False
+    
+    bs_pred, pred = bs_calc(pred, "sum")
+    bs_target, target = bs_calc(target, "sum")
+    _, switch = switch_criterion(bs_pred, bs_target)
+    if switch:
+        pred = torch.flip(pred, dims=(-2,))
+    
+    return pred  
+      
+def switch_criterion(bs_pred, bs_gt):
+    sh = bs_pred.shape
+    reversed_bs_pred = torch.flip(bs_pred, dims=(1,))
+    loss1 = torch.mean(
+                torch.norm((bs_pred - bs_gt).view(sh[0], sh[1], -1), dim=(0, 2)) / \
+                    torch.norm(bs_gt.view(sh[0], sh[1], -1), dim=(0, 2)))
+    loss2 = torch.mean(
+                torch.norm((reversed_bs_pred - bs_gt).view(sh[0], sh[1], -1), dim=(0, 2)) / \
+                    torch.norm(bs_gt.view(sh[0], sh[1], -1), dim=(0, 2)))
+    # Get the index for the minimal loss
+    i = np.argmin(np.array([loss1.item(), loss2.item()]))
+    # Get the minimal loss
+    loss = torch.min(loss1, loss2)
+    switch = (i != 0)
+    
+    return loss, switch  
+  
+# Load the model
 model = get_model(device, args)
 model.load_state_dict(torch.load(model_path)['model_state_dict'])
 model.eval()
 model.to(device)
 
+# Get number of samples
 data_size = len(os.listdir(baseline_data_path))
+bs_calc = BispectrumCalculator(K, N, device).to(device)
+aligner = BatchAligneToReference(device).to(device)
 
 # Create Dataset
 target = read_dataset_from_baseline(baseline_data_path, data_size, K, N)
-target.to(device)
-bs_calc = BispectrumCalculator(K, N, device).to(device)
 source, target = bs_calc(target)
-source=source.to(device)
-target.to(device)
+source = source.to(device)
+target = target.to(device)
 dataset = UnitVecDataset(source, target)
 dataloader = DataLoader(
     dataset=dataset,
@@ -112,38 +141,112 @@ dataloader = DataLoader(
     pin_memory=False,
     shuffle=False
 )
+# Read baseline data from files
 baseline = torch.zeros(data_size, K, N)
 for i in range(data_size):
     folder = os.path.join(baseline_data_path, f'sample{i}')
     for j in range(K):
         baseline[i][j] = read_org(folder, j, K, f"x_est") 
-aligner = BatchAligneToReference(device).to(device)
-avg_err = 0
+
+avg_err = 0.
+avg_min_err_between_signals = 0.
 for idx, (source, target) in dataloader:
     i = idx.item()
     folder_write = os.path.join(output_path, f'sample{i}')
     if not os.path.exists(folder_write):
         os.mkdir(folder_write)
     folder_read = os.path.join(baseline_data_path, f'sample{i}')
-    # pass the baseline samples bispectrum through the model
+    
     source = source.to(device)
-    output = model(source)
     target = target.to(device)
-    output, _ = aligner(output, target)
-    for j in range(K):
-        # set folder to write to
-        folder_k = os.path.join(folder_write, f'{j+1}')
-        if not os.path.exists(folder_k):
-            os.mkdir(folder_k)
-        # Draw the baseline vs model output
-        plot_output_debug2(target.squeeze(0)[j].cpu().detach().numpy(), 
-                               output.squeeze(0)[j].cpu().detach().numpy(),
-                               folder_k,
-                               baseline[i][j])
-    rel_error_X = torch.norm(target - output) / torch.norm(target)
+
+    # Pass the baseline samples' bispectrum through the model
+    output = model(source)
+    
+    if K == 2 and check_k1_k2_distance:
+        # Calculate distance between s0 and s1, to verify the cnn does not learn the same signal
+        # Option 1 - no switch
+        output, _ = aligner(output, target)
+        min_err_between_signals = torch.norm(output.squeeze(0)[0] - output.squeeze(0)[1]) / \
+                                                torch.norm(output.squeeze(0)[0])
+        # Option 2 - switch
+        output = switch_position(output, target, bs_calc)
+        output, _ = aligner(output, target)
+        
+        min_err_between_signals = min(min_err_between_signals, 
+                                      torch.norm(output.squeeze(0)[0] - output.squeeze(0)[1]) / \
+                                      torch.norm(output.squeeze(0)[0]))
+        print(f'min_err_between_signals={min_err_between_signals}')
+        avg_min_err_between_signals += min_err_between_signals
+    else:
+        output = switch_position(output, target, bs_calc)
+        output, _ = aligner(output, target)
+
+
     rel_error_X_path = os.path.join(folder_write, 'rel_error_X.csv')
-    np.savetxt(rel_error_X_path, [rel_error_X.item()])
-    print(f'sample{i}, err={rel_error_X}')        
-    avg_err += rel_error_X
+
+    if K ==2 and check_k1_k2_distance:
+        # Align output 1 to output 0 --> output 1 aligned
+        output_1_aligned, _ = align_to_reference(output.squeeze(0)[1], 
+                                        output.squeeze(0)[0])
+        
+        target_0_aligned, _ = align_to_reference(target.squeeze(0)[0], 
+                                        output.squeeze(0)[0])
+        target_1_aligned, _ = align_to_reference(target.squeeze(0)[1], 
+                                        output_1_aligned)
+        
+        target = torch.stack([target_0_aligned, target_1_aligned], dim=0)
+        output = torch.stack([output.squeeze(0)[0], output_1_aligned], dim=0)
+        rel_error_X = torch.norm(target - output) / torch.norm(target)
+        print(f'sample{i}, err={rel_error_X}')        
+        avg_err += rel_error_X
+        output_avg = (output[0] + output[1]) / 2
+        target_avg = (target[0] + target[1]) / 2
+        fig_path = os.path.join(folder_write, 'comp_s1_pred_s2_pred_preds_avg.jpg')
+        plt.figure(figsize=(9, 5))
+        plt.title(f'Comparison between s1_pred, s2_pred, pred_avg, sample{i}, rel_mse={rel_error_X:.03f}')
+        plt.plot(output[0].cpu().detach().numpy(), label='s1_pred', color='tab:orange')
+        plt.plot(output[1].cpu().detach().numpy(), label='s2_pred', color='tab:red')
+        plt.plot(output_avg.cpu().detach().numpy(), label='avg_pred', color='tab:blue', linestyle='dashed')
+        plt.plot(target_avg.cpu().detach().numpy(), label='target_avg', color='tab:green')
+    
+        plt.ylabel('signal')
+        plt.xlabel('time')
+        plt.legend()
+        plt.savefig(fig_path)        
+        plt.close()
+        fig_path = os.path.join(folder_write, 'comp_s1_s2_s1_pred_s2_pred.jpg')
+    
+        plt.figure(figsize=(9, 5))
+        plt.title(f'Comparison between s1, s2, s1_pred, s2_pred, sample{i}')
+        plt.plot(target[0].cpu().detach().numpy(), label='s1', color='tab:blue')
+        plt.plot(target[1].cpu().detach().numpy(), label='s2', color='tab:green')
+        plt.plot(output[0].cpu().detach().numpy(), label='s1_pred', color='tab:orange', linestyle='dashed')
+        plt.plot(output[1].cpu().detach().numpy(), label='s2_pred', color='tab:red', linestyle='dashed')
+    
+        plt.ylabel('signal')
+        plt.xlabel('time')
+        plt.legend()
+        plt.savefig(fig_path)        
+        plt.close()
+    else:
+        # Plot org, baseline and output for comparison
+        for j in range(K):
+            # set folder to write to
+            folder_k = os.path.join(folder_write, f'{j+1}')
+            if not os.path.exists(folder_k):
+                os.mkdir(folder_k)
+            # Draw the baseline vs model output
+            plot_output_debug2(target.squeeze(0)[j].cpu().detach().numpy(), 
+                                    output.squeeze(0)[j].cpu().detach().numpy(),
+                                    folder_k,
+                                    baseline[i][j])
+        rel_error_X = torch.norm(target - output) / torch.norm(target)
+        np.savetxt(rel_error_X_path, [rel_error_X.item()])
+        print(f'sample{i}, err={rel_error_X}')        
+
+        
 print(f'avg err={(avg_err / data_size):.08f}')        
+if K == 2 and check_k1_k2_distance:
+    print(f'avg min_err_between_signals={(avg_err / data_size):.08f}')        
 
